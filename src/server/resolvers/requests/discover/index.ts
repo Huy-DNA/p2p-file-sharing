383fe703f2e10d88a6c9835c5266ec8ed6727e9f
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-05 19:54:50 +0700

    fix(server): resolve discover request

--- src/server/resolvers/requests/discover/index.ts
+++ src/server/resolvers/requests/discover/index.ts
@@ -1,7 +1,36 @@
 import net from 'net';
+import Joi from 'joi';
 import { DiscoverRequest } from '../../../../common/protocol/requests.js';
+import * as dns from '../../../../common/dns.js';
+import clientStore from '../../../stores/clients.js';
+import { DiscoverResponse, DiscoverStatus, serializeResponse } from '../../../../common/protocol/response.js';
+import { MessageType } from '../../../../common/protocol/types.js';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function resolveDiscoverRequest(connection: net.Socket, discoverRequest: DiscoverRequest) {
- 
+export async function resolveDiscoverRequest(connection: net.Socket, discoverRequest: DiscoverRequest) {
+  const { headers: { hostname } } = discoverRequest;
+  let response: DiscoverResponse | undefined;
+
+  try {
+    const ip = Joi.string().trim().ip().validate(hostname).error ? (await dns.lookup(hostname)).address : hostname;
+    const clientInfo = clientStore.get(ip);
+    if (!clientInfo) {
+      response = {
+        type: MessageType.DISCOVER,
+        status: DiscoverStatus.HOST_NOT_FOUND,
+      };
+    } else {
+      response = {
+        type: MessageType.DISCOVER,
+        status: DiscoverStatus.OK,
+        body: Array.from(clientInfo.files),
+      };
+    }
+  } catch (e) {
+    response = {
+      type: MessageType.DISCOVER,
+      status: DiscoverStatus.HOST_NOT_FOUND,
+    };
+  }
+
+  connection.write(serializeResponse(response));
 }
