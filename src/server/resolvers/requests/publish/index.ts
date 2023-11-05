e388bf399acd444e58be10540a676da7f51eb365
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-05 19:55:32 +0700

    feat(server): resolve publish request

--- src/server/resolvers/requests/publish/index.ts
+++ src/server/resolvers/requests/publish/index.ts
@@ -1,7 +1,27 @@
-import { PublishRequest } from '../../../../common/protocol/requests.js';
 import net from 'net';
+import { PublishResponse, PublishStatus, serializeResponse } from '../../../../common/protocol/response.js';
+import { PublishRequest } from '../../../../common/protocol/requests.js';
+import { MessageType } from '../../../../common/protocol/types.js';
+import clientStore from '../../../../server/stores/clients.js';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function resolvePublishRequest(connection: net.Socket, publishRequest: PublishRequest) {
-  
+  const { headers: { filename } } = publishRequest;
+  const ip = connection.remoteAddress;
+
+  let response: PublishResponse | undefined;
+  if (!ip || !clientStore.get(ip)) {
+    response = {
+      type: MessageType.PUBLISH,
+      status: PublishStatus.BAD_REQUEST,
+    };
+  } else {
+    const clientInfo = clientStore.get(ip)!;
+    clientInfo.files.add(filename);
+    response = {
+      type: MessageType.PUBLISH,
+      status: PublishStatus.OK,
+    };
+  }
+
+  connection.write(serializeResponse(response));
 }
