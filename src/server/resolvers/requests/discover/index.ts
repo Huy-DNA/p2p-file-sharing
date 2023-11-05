ab1220bdeb32ed82c61aa0589206364f4a2ef54c
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-05 16:34:19 +0700

    fix(server): reset discover request resolver

--- src/server/resolvers/requests/discover/index.ts
+++ src/server/resolvers/requests/discover/index.ts
@@ -1,34 +1,7 @@
 import net from 'net';
-import dotenv from 'dotenv';
-import { DiscoverRequest, serializeRequest } from '../../../../common/protocol/requests.js';
-import { DiscoverResponse, DiscoverStatus, serializeResponse } from '../../../../common/protocol/response.js';
-import { MessageType } from '../../../../common/protocol/types.js';
-import { MESSAGE_BOUNDARY } from '../../../../common/constants.js';
+import { DiscoverRequest } from '../../../../common/protocol/requests.js';
 
-dotenv.config();
-
-const { CLIENT_PORT } = process.env;
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function resolveDiscoverRequest(connection: net.Socket, discoverRequest: DiscoverRequest) {
-  let { headers: { hostname } } = discoverRequest;
-  hostname = hostname.trim();
-
-  const socket = net.createConnection({ host: hostname, port: Number.parseInt(CLIENT_PORT!, 10), });
-  socket.write(serializeRequest(discoverRequest));
-  let message = '';
-  socket.on('data', (data) => {
-    const messages = data.toString().split(MESSAGE_BOUNDARY);
-    message += messages[0];
-    if (messages.length > 1) {
-      connection.write(message + MESSAGE_BOUNDARY);
-      socket.end();
-    }
-  });
-  socket.on('error', () => {
-    const response: DiscoverResponse = {
-      status: DiscoverStatus.HOST_NOT_FOUND,
-      type: MessageType.DISCOVER,
-    }
-    connection.write(serializeResponse(response));
-  });
+ 
 }
