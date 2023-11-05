666446a5049db1e29fdc7357f535ef7320187515
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-05 20:45:04 +0700

    refactor: ping

--- src/server/resolvers/requests/ping/index.ts
+++ src/server/resolvers/requests/ping/index.ts
@@ -1,12 +1,12 @@
 import { PingResponse, PingStatus, serializeResponse } from '../../../../common/protocol/response.js';
-import { PingRequest, serializeRequest } from '../../../../common/protocol/requests.js';
+import { PingRequest } from '../../../../common/protocol/requests.js';
 import net from 'net';
 import { MessageType } from '../../../../common/protocol/types.js';
-import { MESSAGE_BOUNDARY } from '../../../../common/constants.js';
+import ping from '../../../utils/ping.js';
 
 const { CLIENT_PORT } = process.env;
 
-export function resolvePingRequest(connection: net.Socket, pingRequest: PingRequest) {
+export async function resolvePingRequest(connection: net.Socket, pingRequest: PingRequest) {
   let { headers: { hostname } } = pingRequest;
   if (hostname === undefined) {
     const response: PingResponse = {
@@ -19,22 +19,6 @@ export function resolvePingRequest(connection: net.Socket, pingRequest: PingRequ
 
   hostname = hostname.trim();
   
-  const socket = net.createConnection({ host: hostname, port: Number.parseInt(CLIENT_PORT!, 10), });
-  socket.write(serializeRequest(pingRequest));
-  let message = '';
-  socket.on('data', (data) => {
-    const messages = data.toString().split(MESSAGE_BOUNDARY);
-    message += messages[0];
-    if (messages.length > 1) {
-      connection.write(message + MESSAGE_BOUNDARY);
-      socket.end();
-    }
-  });
-  socket.on('error', () => {
-    const response: PingResponse = {
-      status: PingStatus.HOST_NOT_FOUND,
-      type: MessageType.PING,
-    }
-    connection.write(serializeResponse(response));
-  });
+  const response = await ping(hostname, parseInt(CLIENT_PORT!, 10));
+  connection.write(serializeResponse(response));
 }
---
+++ src/server/utils/ping.ts
@@ -0,0 +1,36 @@
+import { MESSAGE_BOUNDARY } from '../../common/constants.js';
+import { PingRequest, serializeRequest } from '../../common/protocol/requests.js';
+import { PingResponse, PingStatus, deserializeResponse } from '../../common/protocol/response.js';
+import { MessageType } from '../../common/protocol/types.js';
+import { extractPingResponse } from '../../common/protocol/validators/response.js';
+import net from 'net';
+
+export default async function ping(hostname: string, port: number): Promise<PingResponse> {
+  const pingRequest: PingRequest = {
+    type: MessageType.PING,
+    headers: {},
+  };
+
+  return new Promise((resolve) => {
+    const socket = net.createConnection({ host: hostname, port, });
+    socket.write(serializeRequest(pingRequest));
+
+    let message = '';
+    socket.on('data', (data) => {
+      const messages = data.toString().split(MESSAGE_BOUNDARY);
+      message += messages[0];
+      if (messages.length > 1) {
+        socket.end();
+        const response = deserializeResponse(message).and_then(extractPingResponse).unwrap();
+        resolve(response);
+      }
+    });
+    socket.on('error', () => {
+      const response: PingResponse = {
+        status: PingStatus.HOST_NOT_FOUND,
+        type: MessageType.PING,
+      }
+      resolve(response);
+    });
+  });
+}
