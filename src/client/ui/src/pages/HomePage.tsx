e2cea07d61646127827f3ac391bf02c0b38279b6
quannhg <quan.nguyenhg12@gmail.com>
2023-11-07 22:00:18 +0700

    feat(home): add announce button, fix fetch in discover page, change port of gui to 3135

--- src/client/ui/src/pages/HomePage.tsx
+++ src/client/ui/src/pages/HomePage.tsx
@@ -1,10 +1,24 @@
-import { Typography } from "@material-tailwind/react";
+import { Button, Typography } from "@material-tailwind/react";
 
 export function HomePage() {
+  const handleAnnounceButton = () => {
+    console.log("announce to server");
+  };
   return (
     <>
-      <Typography variant="h3" className="pb-4">This is home page</Typography>
-      <Typography variant="paragraph">Of p2p file sharing</Typography>
+      <Typography variant="h3" className="pb-4">
+        P2P File Sharing Application
+      </Typography>
+      <Typography variant="paragraph" className="pb-4">
+        Press to connect to Server
+      </Typography>
+      <Button
+        onClick={handleAnnounceButton}
+        variant="gradient"
+        className="rounded-none capitalize w-fit py-2"
+      >
+        Announce
+      </Button>
     </>
   );
 }
